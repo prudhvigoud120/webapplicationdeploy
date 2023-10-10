4f810e3263f39ba6317e9e7d5c1a0f3b151bf594
prudhvigoud120 <145613393+prudhvigoud120@users.noreply.github.com>
2023-10-10 13:56:05 -0400

    Update and rename NiceJob.js to nicejob.js

--- prudhvi-app2/nicejob.js
+++ prudhvi-app2/nicejob.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/NiceJob', (req, res) => {
-  res.send("This is prudhvi's NiceJob!")
+app.get('/nicejob', (req, res) => {
+  res.send("This is prudhvi's nicejob!")
 })
 
 app.listen(port, () => {
